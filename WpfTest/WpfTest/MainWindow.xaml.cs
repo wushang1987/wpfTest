65f7146c227d097aa3cbbf88f1b5fb558ea14198
wushang1987@gmail.com <wushang1987@gmail.com>
2015-12-22 00:24:21 +0800

    获取文件夹，设置工作空间

--- WpfTest/WpfTest/MainWindow.xaml.cs
+++ WpfTest/WpfTest/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Forms;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -18,11 +19,25 @@ namespace WpfTest
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.Description = "请选择文件夹";
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                workSpaceText.Content = dialog.SelectedPath;
+                workSpaceButton.Content = "修改工作空间";
+            }
+
         }
     }
 }
